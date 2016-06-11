269eadea7bf7330da04aaa1fcff12d6037fc9f7d
Tianqing <442034859@qq.com>
2016-06-11 18:03:01 +0800

    adding third party gem [chronic,colorize], adding Gemfile, new_list.all NilClass bug fix

--- lib/todo.rb
+++ lib/todo.rb
@@ -4,7 +4,7 @@ class TodoItem
   @@priorities = ["high","medium","low",nil]
   def initialize(description, options={})
     @description = description
-    @due = options[:due] ? Date.parse(options[:due]) : options[:due]
+    @due = options[:due] ? Date.parse(Chronic.parse(options[:due]).to_s) : options[:due]
     @priority = options[:priority]
     if !@@priorities.include?(@priority)
       raise UdaciListErrors::InvalidPriorityValueError, "We don't have such a '#{@priority}' priority option"
